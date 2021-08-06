50abe29b92ff3ffc39b9820506db48ab1a22dd19
James Benjamin Pawlik <james.benjamin.pawlik@gmail.com>
2021-08-06 10:57:08 -0700

    create Review validations; specs passing

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,9 @@
 class Review < ApplicationRecord
   belongs_to :product
+  
+  validates :author, presence: true
+  validates :content_body, presence: true, length: { minimum: 50, maximum: 250}
+  validates :rating, presence: true
+  validates_numericality_of :rating, less_than: 6, greater_than: 0
+
 end
