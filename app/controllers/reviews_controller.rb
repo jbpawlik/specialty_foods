e46335eb968cb4d35544648de7c955e7c4f51f35
James Benjamin Pawlik <james.benjamin.pawlik@gmail.com>
2021-08-06 10:25:31 -0700

    add reviews.controller.rb

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -14,6 +14,34 @@ class ReviewsController < ApplicationController
       render :new
     end
 
+    def show 
+      @product = Product.find(params[:product_id])
+      @review = Review.find(params[:id])
+      render :show
+    end
+
+    def edit
+      @product = Product.find(params[:product_id])
+      @review = Review.find(params[:id])
+      render :edit
+    end
+
+    def update
+      @review = Review.find(params[:id])
+      if @review.update(review_params)
+        redirect_to product_path(@review.product)
+      else
+        @product = Product.find(params[:product_id])
+        render :edit
+      end
+    end
+
+    def destroy
+      @review = Review.find(params[:id])
+      @review.destroy
+      redirect_to product_path(@review.product)
+    end
+
   private
     def review_params
       params.require(:review).permit(:author, :content_body, :rating)
